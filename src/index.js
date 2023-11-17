1220cf0cb37776b0347a16479a63b1c1db123c84
Maciej Marchel <maciejmarchel14@gmail.com>
2023-11-17 13:15:06 +0000

    Optimize main bundle's size

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes, Navigate, Link } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "react-query";
@@ -6,7 +6,7 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import HomePage from "./pages/homePage";
 import MovieReviewersPage from "./pages/reviewersPage";
 import "./style.css";
-import MoviePage from "./pages/movieDetailsPage";
+const MoviePage = lazy(() => import("./pages/movieDetailsPage"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,6 +23,7 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Link to="/">Home</Link>
+        <Suspense fallback={<h1>Loading page</h1>}>
           <Routes>
             <Route
               path="/movies/:id/reviewers"
@@ -32,6 +33,7 @@ const App = () => {
             <Route exact path="/" element={<HomePage />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
+          </Suspense>
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
